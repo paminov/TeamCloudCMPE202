2744ab0ab2a42f3038034097fe6729c346196356
Navjot Bola <navjotb1@gmail.com>
2019-05-08 20:31:06 -0700

    End to end order view/ api logic

--- ui/src/containers/Order/Order.js
+++ ui/src/containers/Order/Order.js
@@ -11,13 +11,15 @@ export default class Order extends Component {
             menu: [],
             cart: [],
             totalCost: 0,
-            checkingOut: false
+            checkingOut: false,
+            balance: null,
         };
     }
 
     async componentDidMount() {
         this.getMenuItems()
         this.fetchCart()
+        this.getCard();
     }
 
     async getMenuItems() {
@@ -47,49 +49,114 @@ export default class Order extends Component {
         return API.get("","/cart")
     }
 
-    addToCart = (index) => {
+    async getCard() {
+        try {
+            const card = await this.card();
+            if (card) {
+                this.setState({
+                    balance: card.balance,
+                });
+            }
+        } catch (e) {
+            alert(e);
+        }
+        this.setState({ isLoading: false });
+    }
+
+    card() {
+        return API.get("","/card")
+    }
+
+    addToCart = async (index) => {
         const { menu } = this.state;
         console.log(index);
         const item = menu[index];
-        API.patch("","/cart", {
+        const cart = await API.patch("","/cart", {
             body: item
         })
-        this.fetchCart();
+        this.setState({
+            totalCost: cart.totalCost,
+            cart: cart.menuItems,
+        })
     }
 
-    endCheckout = () => {
+
+    removeFromCart = async (index) => {
+        const cart = await API.del("",`/cart/${index}`)
+        this.setState({
+            totalCost: cart.totalCost,
+            cart: cart.menuItems,
+        })
+    }
+
+    endCheckout = async (type) => {
+        if (type === 'cancel') {
+            this.setState({ checkingOut: false });
+            return
+        }
+        await this.processPayment();
         this.setState({ checkingOut: false });
-      }
-    
+    }
+
+    async processPayment() {
+        try {
+            await this.pay();
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+    pay() {
+        return API.post("","/transactions")
+    }
+
     showCheckout = () => {
         console.log('showcheckout')
         this.setState({ checkingOut: true });
     }
 
     renderMenu() {
-        const { menu } = this.state;
+        const { menu, totalCost, balance, cart } = this.state;
         return menu.map((item, i) =>
-            <tr>
-                <td>{item.name.toLowerCase()}</td>
-                <td>{item.cost}</td>
-                <td class="addToCart"><Button bsStyle="success" onClick={() => this.addToCart(i)}><i className="fas fa-plus fa-fw"></i>Add to cart</Button></td>
-            </tr>
+            {
+                const noFundsRemaining = Number(balance) < (Number(totalCost) + Number(item.cost));
+                let isInCart = cart.findIndex(cartItem => cartItem.name === item.name);
+                return (<tr>
+                    <td>{item.name.toLowerCase()}</td>
+                    <td>{item.cost}</td>
+                    <td class="addToCart"><Button bsStyle="success" onClick={() => this.addToCart(i)} disabled={noFundsRemaining || isInCart !== -1}><i className="fas fa-plus fa-fw"></i>Add to cart</Button></td>
+                    <td class="addToCart"><Button bsStyle="danger" onClick={() => this.removeFromCart(isInCart)} disabled={isInCart === -1}><i className="fas fa-minus fa-fw"></i>Delete</Button></td>
+                </tr>)
+            }
         );
     }
 
     render() {
-        const { isLoading, cart, totalCost } = this.state;
+        const { isLoading, cart, totalCost, balance } = this.state;
+        let cost = totalCost.toFixed(2);
+        if(cost == -0.00){
+            cost = 0.00;
+        }
+          
         const modal = (
            <Modal show={this.state.checkingOut} onHide={this.endCheckout}>
                 <Modal.Header closeButton>
                 <Modal.Title>Checkout</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Pay Here</Modal.Body>
+                <Modal.Body className="grid center">
+                    {
+                        cart.map((item) => {
+                          return <span>{item.name}</span>
+                        })
+                    }
+                    <hr/>
+                    ${ cost.toFixed(2) }
+                </Modal.Body>
                 <Modal.Footer>
-                <Button variant="secondary" onClick={this.endCheckout}>
+                <Button bsStyle="secondary" onClick={() => this.endCheckout('cancel')}>
                     Cancel
                 </Button>
-                <Button variant="primary" onClick={this.endCheckout}>
+                <Button bsStyle="primary" onClick={this.endCheckout}>
                     Pay
                 </Button>
                 </Modal.Footer>
@@ -100,8 +167,11 @@ export default class Order extends Component {
                 {!isLoading && 
                 <div className="menu-holder">
                     <span className="shopping-cart pull-right">
-                        <i className="fas fa-shopping-cart fa-fw" />{cart.length} items ($ {totalCost})
+                        <i className="fas fa-shopping-cart fa-fw" />{cart.length} items ($ {cost.toFixed(2)})
                         <Button onClick={this.showCheckout} className="button-ml" bsStyle="warning" disabled={cart.length === 0}>Checkout</Button>
+                    </span>
+                    <span className="shopping-cart pull-right mt-5">
+                        <i className="fas fa-credit-card fa-fw" />$ {Number(balance).toFixed(2)} balance
                     </span>
                     <Table borderless hover variant="dark">
                         <thead>
